8a912af81d104e75f24195c90630c36df264788b
Theo <100994644+theokarli@users.noreply.github.com>
2023-06-07 16:37:14 +0800

    Update sw.js
    kembalikan sw seperti semula, pakai tipe cache first

--- PWA/sw.js
+++ PWA/sw.js
@@ -23,98 +23,45 @@ self.addEventListener('install', function(event) {
 
 // Setelah kita install Service Worker, kita akan melakukan Fetching dan Activate Cache, sehingga kita bisa menggunakan dokumen yg kita simpan di cache secara offline.
 // Fetching (Kita Fetch/Ambil data dari cache storage yg sudah kita buat, jika ada kita ambil):
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
-
-//         return fetch(event.request).then(
-//           function(response) {
-//             // Check if we received a valid response
-//             if(!response || response.status !== 200 || response.type !== 'basic') {
-//               return response;
-//             }
-
-//             // IMPORTANT: Clone the response. A response is a stream
-//             // and because we want the browser to consume the response
-//             // as well as the cache consuming the response, we need
-//             // to clone it so we have two streams.
-//             var responseToCache = response.clone();
-
-//             caches.open(CACHE_NAME)
-//               .then(function(cache) {
-//                 cache.put(event.request, responseToCache);
-//               });
-
-//             return response;
-//           }
-//         )
-//       }).catch(function () {  // Untuk menampilkan offline.html ketika tidak ada koneksi internet
-//         // If both fail, show a generic fallback:
-//         return caches.match('./offline.html');
-//         // However, in reality you'd have many different
-//         // fallbacks, depending on URL and headers.
-//         // Eg, a fallback silhouette image for avatars.
-//       })
-//     );
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request)
+    caches.match(event.request)
       .then(function(response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+        // Cache hit - return response
+        if (response) {
           return response;
         }
 
-        // IMPORTANT: Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-
-        return response;
-      })
-      .catch(function() {
-        return caches.match(event.request)
-          .then(function(response) {
-            if (response) {
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-            return caches.match('./offline.html');
-          });
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        )
+      }).catch(function () {  // Untuk menampilkan offline.html ketika tidak ada koneksi internet
+        // If both fail, show a generic fallback:
+        return caches.match('./offline.html');
+        // However, in reality you'd have many different
+        // fallbacks, depending on URL and headers.
+        // Eg, a fallback silhouette image for avatars.
       })
-  );
+    );
 });
-
-
-
-
-
-
-
 
 
 // Activate Cache (Setelah kita Fetch data dari cache storage, kita akan aktifkan dan menggunakan cache storage untuk web kita. Sehingga kita bisa menggunakan dokumen yg kita simpan di cache secara offline):
